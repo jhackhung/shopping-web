8c196fd50dc229bf4ba1f08e9e358d91f6860042
Jack <hungjack1222@gmail.com>
2024-02-25 01:03:55 +0800

    Feat: Set CORS Header in Backend To connect with trusted origin

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const userRouter = require('./routes/userRoutes');
-const prodRouter = require('./routes/prodRoutes');
-const checkoutRouter = require('./routes/checkoutRoutes');
+const userRouter = require("./routes/userRoutes");
+const prodRouter = require("./routes/prodRoutes");
+const checkoutRouter = require("./routes/checkoutRoutes");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api/v1/prods', prodRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/checkout', checkoutRouter);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.TRUSTED_ORIGIN);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/api/v1/prods", prodRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/checkout", checkoutRouter);
 
 module.exports = app;
--- backend/routes/prodRoutes.js
+++ backend/routes/prodRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const prodController = require("../controllers/prodController");
 const router = express.Router();
 
-router.route('/').get(prodController.getProducts).post(prodController.createProduct);
-router.route('/:id').get();
+router.route("/").get(prodController.getProducts).post(prodController.createProduct);
+router.route("/:id").get();
 
 module.exports = router;
